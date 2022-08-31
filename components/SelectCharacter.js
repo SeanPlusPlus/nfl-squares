037438034c88aa50d8df751d71451211dd449910
Sean Stephenson <sean.m.stephenson@disney.com>
2022-08-31 16:11:47 -0700

    main: connect wallet

---
+++ components/SelectCharacter.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+/*
+ * Don't worry about setCharacterNFT just yet, we will talk about it soon!
+ */
+const SelectCharacter = ({ setCharacterNFT }) => {
+  return (
+    <div className="select-character-container">
+      <h2>Mint Your Hero. Choose wisely.</h2>
+    </div>
+  );
+};
+
+export default SelectCharacter;
--- pages/nft.js
+++ pages/nft.js
@@ -1,38 +1,106 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // components
 import Header from '../components/header'
 import Nav from '../components/nav'
+import SelectCharacter from '../components/SelectCharacter';
 
 // Constants
 const TWITTER_HANDLE = 'SeanPlusPlus';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const NFT = () => {
-  /*
-   * Start by creating a new action that we will run on component load
-   */
-  // Actions
-  const checkIfWalletIsConnected = () => {
-    /*
-     * First make sure we have access to window.ethereum
-     */
-    const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [characterNFT, setCharacterNFT] = useState(null);
 
-    if (!ethereum) {
-      console.log('Make sure you have MetaMask!');
-      return;
-    } else {
-      console.log('We have the ethereum object', ethereum);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  /*
-   * This runs our function when the page loads.
-   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
+
+  const renderContent = () => {
+    /*
+     * Scenario #1
+     */
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+          <img
+            src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+            alt="Monty Python Gif"
+          />
+          <button
+            className="btn mt-2"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+      /*
+       * Scenario #2
+       */
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    }
+  };
 
   return (
 
@@ -48,20 +116,15 @@ const NFT = () => {
                 <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
                 <p className="sub-text">Team up to protect the Metaverse!</p>
               </div>
-              <div className="connect-wallet-container">
-                <img
-                  src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-                  alt="Monty Python Gif"
-                />
-              </div>
-              <div className="footer-container">
-                <img alt="Twitter Logo" className="twitter-logo" src='/twitter.svg' />
+              {renderContent()}
+              <div className="flex mt-2">
+                <img alt="Twitter Logo" className="h-12 pt-2" src='/twitter.svg' />
                 <a
-                  className="footer-text"
+                  className="pt-4"
                   href={TWITTER_LINK}
                   target="_blank"
                   rel="noreferrer"
-                >{`built with @${TWITTER_HANDLE}`}</a>
+                >{`built by @${TWITTER_HANDLE}`}</a>
               </div>
             </div>
           </div>
