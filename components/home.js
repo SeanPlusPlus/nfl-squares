f49ee4296f520c707a1f7980d1f0990f778ecc0f
Sean Stephenson <sean.m.stephenson@disney.com>
2022-08-31 14:18:18 -0700

    main: about on hime

--- components/home.js
+++ components/home.js
@@ -1,16 +1,43 @@
+import { useState } from 'react'
+
+// components
+import About from './about'
+
 const Home = () => {
+  const [modalAbout, setModalAbout] = useState('')
+
+  const handleOpenAbout = () => {
+    setModalAbout('modal-open')
+  }
+
+  const handleCloseAbout = () => {
+    setModalAbout('')
+  }
+
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center mb-64">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">NFL Squares</h1>
-          <p className="py-6">
-            A game very similar to Super Bowl Squares, but this one runs for the entire NFL season!!!
-          </p>
-          <button className="btn btn-primary">About</button>
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center mb-64">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">NFL Squares</h1>
+            <p className="py-6">
+              A game very similar to Super Bowl Squares, but this one runs for the entire NFL season!!!
+            </p>
+            <button className="btn btn-primary" onClick={handleOpenAbout}>About</button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className={`modal ${modalAbout}`}>
+        <div className="modal-box relative">
+          <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={handleCloseAbout}>✕</label>
+          <About />
+          <div className="modal-action pt-5">
+            <label htmlFor="my-modal" className="btn" onClick={handleCloseAbout}>Close</label>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- components/nav.js
+++ components/nav.js
@@ -2,6 +2,9 @@ import Link from 'next/link'
 import { useContext, useState } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
+// components
+import About from './about'
+
 const Nav = () => {
   const [modalAbout, setModalAbout] = useState('')
   const [modalProfile, setModalProfile] = useState('')
@@ -106,19 +109,7 @@ const Nav = () => {
       <div className={`modal ${modalAbout}`}>
         <div className="modal-box relative">
           <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={handleCloseAbout}>✕</label>
-          <h3 className="font-bold text-xl flex">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            <span className="ml-1 text-xl mb-4">
-              About
-            </span>
-          </h3>
-          <div>
-            <p className="pb-4">
-              Like Super Bowl Squares ... But every week of the season !!!
-            </p>
-          </div>
+          <About />
           <div className="modal-action pt-5">
             <label htmlFor="my-modal" className="btn" onClick={handleCloseAbout}>Close</label>
           </div>
