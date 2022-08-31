8edd3f0b486318bdba6ad3273638358eca9d9fe9
Sean Stephenson <sean.m.stephenson@disney.com>
2022-08-31 15:53:43 -0700

    main: add nft

--- pages/nft.js
+++ pages/nft.js
@@ -1,16 +1,15 @@
 // components
 import Header from '../components/header'
 import Nav from '../components/nav'
-import Board from '../components/board'
 
-export default function Play() {
+export default function NFT() {
   return (
     <div className="grid-bg min-h-screen">
       <Header />
       <Nav />
       <main className="flex md:mt-12">
         <div className="m-auto">
-          <Board />
+          NFT
         </div>
       </main>
     </div>
